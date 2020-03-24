dbae6be5a515720d2503ccd02f434174c4cfb1ed
Andrey Filippochkin <voometin@yandex.ru>
2020-03-24 13:36:46 +0300

    Fixed cross_validation.py for LGB and Catboost Classifiers

--- cross_validation.py
+++ cross_validation.py
@@ -26,30 +26,19 @@ def cross_validation(cv, model, features, target, metrics=(roc_auc_score,), verb
     scores = {}
     for metric in metrics:
         scores[metric.__name__] = {'train': [], 'val': []}
-    modeltype = train_params.pop('modeltype', None)
-    catboost_cat_features = train_params.pop('catboost_cat_features', None)
+    # modeltype = train_params.pop('modeltype', None)
 
     for train_index, val_index in cv.split(features, target):
         train_features, val_features = features[train_index], features[val_index]
         train_target, val_target =  target[train_index], target[val_index]
 
-        if modeltype in ['lgb', 'catboost']:
-            if modeltype == 'lgb':
-                train_dataset = lgb.Dataset(train_features, train_target, free_raw_data=False)
-                val_dataset = lgb.Dataset(val_features, val_target, free_raw_data=False)
+        if type(model) in [lgb.LGBMClassifier, catboost.CatBoostClassifier]:
+            train_params['fit_params'].update({'eval_set': [(val_features, val_target)]})
 
-            elif modeltype == 'catboost':
-                train_dataset = catboost.Pool(train_features, train_target, cat_features=catboost_cat_features,
-                                              feature_names=list(train_features.columns), thread_count=1)
-                val_dataset = catboost.Pool(val_features, val_target, cat_features=catboost_cat_features,
-                                            feature_names=list(train_features.columns), thread_count=1)
-            model.fit(train_dataset, eval_set=val_dataset, **train_params['fit_params'])
-
-        else:  # any other sklearn-like interface model
-            if 'fit_params' in train_params:
-                model.fit(train_features, train_target, **train_params['fit_params'])
-            else:
-                model.fit(train_features, train_target)
+        if 'fit_params' in train_params:
+            model.fit(train_features, train_target, **train_params['fit_params'])
+        else:
+            model.fit(train_features, train_target)
 
         train_predictions_proba = model.predict_proba(train_features).T[1]
         val_predictions_proba = model.predict_proba(val_features).T[1]
@@ -82,6 +71,7 @@ def cross_validation(cv, model, features, target, metrics=(roc_auc_score,), verb
                 print(f"{key.upper()}: {scores[metric.__name__][key]} ({scores[metric.__name__][key + '_mean']})")
 
     return scores, model
+
 
 if __name__ == '__main__':
     print(cross_validation.__doc__)
